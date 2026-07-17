56ea1e0744e267f22c07022e89b19cd9d3f0c11b
Diego Sanchez Del Rio Gonzalez <diegosdr3@gmail.com>
2026-07-17 12:00:22 -0400

    Add simple temperature test script for new ESP32

--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -2,82 +2,37 @@
 #include <OneWire.h>
 #include <DallasTemperature.h>
 
-// --- Motor Pins ---
-const int ENA = 14;
-const int IN1 = 27;
-const int IN2 = 26;
-const int ENB = 32;
-const int IN3 = 33;
-const int IN4 = 25;
+// Temperature sensor data wire is connected to GPIO 4
+#define ONE_WIRE_BUS 4
 
-const int freq = 2000;
-const int resolution = 8;
-const int pwmChannelA = 0;
-const int pwmChannelB = 1;
-
-// --- Temperature Sensor ---
-const int ONE_WIRE_BUS = 4; // GPIO 4 on ESP32
 OneWire oneWire(ONE_WIRE_BUS);
 DallasTemperature sensors(&oneWire);
 
-void setMotors(int speedA, int speedB) {
-  speedA = constrain(speedA, -255, 255);
-  speedB = constrain(speedB, -255, 255);
-
-  if (speedA > 0) {
-    digitalWrite(IN1, HIGH); digitalWrite(IN2, LOW);
-  } else if (speedA < 0) {
-    digitalWrite(IN1, LOW); digitalWrite(IN2, HIGH);
-  } else {
-    digitalWrite(IN1, LOW); digitalWrite(IN2, LOW);
-  }
-  
-  if (speedB > 0) {
-    digitalWrite(IN3, HIGH); digitalWrite(IN4, LOW);
-  } else if (speedB < 0) {
-    digitalWrite(IN3, LOW); digitalWrite(IN4, HIGH);
-  } else {
-    digitalWrite(IN3, LOW); digitalWrite(IN4, LOW);
-  }
-
-  ledcWrite(pwmChannelA, abs(speedA));
-  ledcWrite(pwmChannelB, abs(speedB));
-}
-
 void setup() {
+  // Start serial communication at 115200 baud
   Serial.begin(115200);
+  Serial.println("ESP32 Temperature Sensor Test Starting...");
   
-  pinMode(IN1, OUTPUT); pinMode(IN2, OUTPUT);
-  pinMode(IN3, OUTPUT); pinMode(IN4, OUTPUT);
-
-  ledcSetup(pwmChannelA, freq, resolution);
-  ledcSetup(pwmChannelB, freq, resolution);
-  ledcAttachPin(ENA, pwmChannelA);
-  ledcAttachPin(ENB, pwmChannelB);
-
-  setMotors(0, 0);
+  // Initialize the sensor
   sensors.begin();
-  
-  Serial.println("ESP32 Ready (Motors + Temp)");
 }
 
 void loop() {
-  if (Serial.available() > 0) {
-    String command = Serial.readStringUntil('\n');
-    command.trim();
-
-    if (command == "FWD") {
-      setMotors(200, 200);
-      Serial.println("ACK: FWD");
-    } 
-    else if (command == "STOP") {
-      setMotors(0, 0);
-      Serial.println("ACK: STOP");
-    }
-    else if (command == "TEMP") {
-      sensors.requestTemperatures();
-      float tempC = sensors.getTempCByIndex(0);
-      Serial.println(String(tempC));
-    }
+  // Request temperature readings
+  sensors.requestTemperatures(); 
+  
+  // Read the temperature in Celsius
+  float tempC = sensors.getTempCByIndex(0);
+  
+  // Check if reading was successful
+  if (tempC != DEVICE_DISCONNECTED_C) {
+    Serial.print("Temperature: ");
+    Serial.print(tempC);
+    Serial.println(" C");
+  } else {
+    Serial.println("Error: Could not read temperature data (Sensor disconnected?)");
   }
+  
+  // Wait 1 second before reading again
+  delay(1000);
 }
